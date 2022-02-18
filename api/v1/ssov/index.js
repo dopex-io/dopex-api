3f76afab85be05f06887609e8620f7f04db43756
WitherBlock <witherblock@protonmail.com>
2022-02-18 05:50:30 +0400

    fix: ssov index route

--- api/v1/ssov/index.js
+++ api/v1/ssov/index.js
@@ -1,6 +1,6 @@
 const groupBy = require("lodash/groupBy");
 
-const { SSOVS, SSOV_PUTS } = require("../../../helpers/constants");
+const { SSOVS } = require("../../../helpers/constants");
 const getSsovApy = require("../../../helpers/getSsovApy");
 const getSsovTvl = require("../../../helpers/getSsovTvl");
 
@@ -20,7 +20,7 @@ module.exports = async (req, res) => {
 
     const data = SSOVS.map((item, index) => {
       return { ...item, tvl: tvls[index].toString(), apy: apys[index] };
-    }).concat(SSOV_PUTS);
+    });
 
     const fData = groupBy(data, "chainId");
 
