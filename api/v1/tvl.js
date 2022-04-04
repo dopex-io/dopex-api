c67d1c7d400a3db60e54d52a8a85866cdcfa2f3d
hibou-de-nuit <hp@dopex.io>
2022-04-04 04:49:41 +0400

    feat: metis ssov api

--- api/v1/tvl.js
+++ api/v1/tvl.js
@@ -1,48 +1,52 @@
-import BN from "bignumber.js";
+import BN from 'bignumber.js';
 
-import getFarmTvl from "../../helpers/getFarmTvl";
-import getSsovTvl from "../../helpers/getSsovTvl";
-import getPrice from "../../helpers/getPrice";
-import { BLOCKCHAIN_TO_CHAIN_ID } from "../../helpers/constants";
+import getFarmTvl from '../../helpers/getFarmTvl';
+import getSsovTvl from '../../helpers/getSsovTvl';
+import getPrice from '../../helpers/getPrice';
+import { BLOCKCHAIN_TO_CHAIN_ID } from '../../helpers/constants';
 
 export default async (req, res) => {
   try {
     let tvl = 0;
-    const { usd: ethPriceFinal } = await getPrice("ethereum");
+    const { usd: ethPriceFinal } = await getPrice('ethereum');
 
     const INCLUDE_QUERY_MAPPING = {
-      "dpx-ssov": {
+      'dpx-ssov': {
         fn: getSsovTvl,
-        args: ["DPX", "call", BLOCKCHAIN_TO_CHAIN_ID["ARBITRUM"]],
+        args: ['DPX', 'call', BLOCKCHAIN_TO_CHAIN_ID['ARBITRUM']],
       },
-      "rdpx-ssov": {
+      'rdpx-ssov': {
         fn: getSsovTvl,
-        args: ["RDPX", "call", BLOCKCHAIN_TO_CHAIN_ID["ARBITRUM"]],
+        args: ['RDPX', 'call', BLOCKCHAIN_TO_CHAIN_ID['ARBITRUM']],
       },
-      "gohm-ssov": {
+      'gohm-ssov': {
         fn: getSsovTvl,
-        args: ["GOHM", "call", BLOCKCHAIN_TO_CHAIN_ID["ARBITRUM"]],
+        args: ['GOHM', 'call', BLOCKCHAIN_TO_CHAIN_ID['ARBITRUM']],
       },
-      "gmx-ssov": {
+      'gmx-ssov': {
         fn: getSsovTvl,
-        args: ["GMX", "call", BLOCKCHAIN_TO_CHAIN_ID["ARBITRUM"]],
+        args: ['GMX', 'call', BLOCKCHAIN_TO_CHAIN_ID['ARBITRUM']],
       },
-      "eth-ssov": {
+      'eth-ssov': {
         fn: getSsovTvl,
-        args: ["ETH", "call", BLOCKCHAIN_TO_CHAIN_ID["ARBITRUM"]],
+        args: ['ETH', 'call', BLOCKCHAIN_TO_CHAIN_ID['ARBITRUM']],
       },
-      "bnb-ssov": {
+      'bnb-ssov': {
         fn: getSsovTvl,
-        args: ["BNB", "call", BLOCKCHAIN_TO_CHAIN_ID["BINANCE"]],
+        args: ['BNB', 'call', BLOCKCHAIN_TO_CHAIN_ID['BINANCE']],
       },
-      "dpx-farm": { fn: getFarmTvl, args: ["DPX", ethPriceFinal] },
-      "rdpx-farm": { fn: getFarmTvl, args: ["RDPX", ethPriceFinal] },
-      "dpx-weth-farm": { fn: getFarmTvl, args: ["DPX-WETH", ethPriceFinal] },
-      "rdpx-weth-farm": { fn: getFarmTvl, args: ["RDPX-WETH", ethPriceFinal] },
+      'metis-ssov': {
+        fn: getSsovTvl,
+        args: ['Metis', 'call', BLOCKCHAIN_TO_CHAIN_ID['METIS']],
+      },
+      'dpx-farm': { fn: getFarmTvl, args: ['DPX', ethPriceFinal] },
+      'rdpx-farm': { fn: getFarmTvl, args: ['RDPX', ethPriceFinal] },
+      'dpx-weth-farm': { fn: getFarmTvl, args: ['DPX-WETH', ethPriceFinal] },
+      'rdpx-weth-farm': { fn: getFarmTvl, args: ['RDPX-WETH', ethPriceFinal] },
     };
 
     const promises = req.query.include
-      ? req.query.include.split(",")
+      ? req.query.include.split(',')
       : Object.keys(INCLUDE_QUERY_MAPPING);
 
     const data = await Promise.all(
@@ -58,12 +62,12 @@ export default async (req, res) => {
       return acc;
     }, new BN(0));
 
-    res.setHeader("Cache-Control", "s-maxage=60, stale-while-revalidate");
+    res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate');
     res.json({ tvl });
   } catch (err) {
     console.log(err);
     res
       .status(500)
-      .json({ error: "Something went wrong.", details: err["reason"] });
+      .json({ error: 'Something went wrong.', details: err['reason'] });
   }
 };
--- helpers/getProvider.js
+++ helpers/getProvider.js
@@ -1,30 +1,38 @@
-import { ethers } from "ethers";
-import { providers } from "@0xsequence/multicall";
-import { BLOCKCHAIN_TO_CHAIN_ID } from "../helpers/constants";
+import { ethers } from 'ethers';
+import { providers } from '@0xsequence/multicall';
+import { BLOCKCHAIN_TO_CHAIN_ID } from '../helpers/constants';
 
 export default (chainId) => {
   const infuraProjectId = process.env.INFURA_PROJECT_ID;
   const bscRpcUrl = process.env.BSC_RPC_URL;
   const avaxRpxUrl = process.env.AVAX_RPC_URL;
-  if (chainId === BLOCKCHAIN_TO_CHAIN_ID["ARBITRUM"])
+  const metisRpcUrl = process.env.METIS_RPC_URL;
+  if (chainId === BLOCKCHAIN_TO_CHAIN_ID['ARBITRUM'])
     return new providers.MulticallProvider(
       new ethers.getDefaultProvider(
         `https://arbitrum-mainnet.infura.io/v3/${infuraProjectId}`,
-        "any"
+        'any'
       )
     );
-  else if (chainId === BLOCKCHAIN_TO_CHAIN_ID["BINANCE"])
+  else if (chainId === BLOCKCHAIN_TO_CHAIN_ID['BINANCE'])
     return new providers.MulticallProvider(
       new ethers.providers.JsonRpcProvider(
         bscRpcUrl,
-        BLOCKCHAIN_TO_CHAIN_ID["BINANCE"]
+        BLOCKCHAIN_TO_CHAIN_ID['BINANCE']
       )
     );
-  else if (chainId === BLOCKCHAIN_TO_CHAIN_ID["AVAX"])
+  else if (chainId === BLOCKCHAIN_TO_CHAIN_ID['AVAX'])
     return new providers.MulticallProvider(
       new ethers.providers.JsonRpcProvider(
         avaxRpxUrl,
-        BLOCKCHAIN_TO_CHAIN_ID["AVAX"]
+        BLOCKCHAIN_TO_CHAIN_ID['AVAX']
+      )
+    );
+  else if (chainId === BLOCKCHAIN_TO_CHAIN_ID['METIS'])
+    return new providers.MulticallProvider(
+      new ethers.providers.JsonRpcProvider(
+        metisRpcUrl,
+        BLOCKCHAIN_TO_CHAIN_ID['METIS']
       )
     );
 };
