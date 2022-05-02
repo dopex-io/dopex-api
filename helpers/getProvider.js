bff92c632feb6b61e4269123c7e6d6d437a22197
hellodespereaux <hellodespereaux@gmail.com>
2022-05-02 22:20:00 +0200

    fix: speedy node offline

--- helpers/getProvider.js
+++ helpers/getProvider.js
@@ -3,14 +3,14 @@ import { providers } from '@0xsequence/multicall'
 import { BLOCKCHAIN_TO_CHAIN_ID } from '../helpers/constants'
 
 export default (chainId) => {
-    const infuraProjectId = process.env.INFURA_PROJECT_ID
+    const arbitrumRpcUrl = process.env.ARBITRUM_RPC_URL
     const bscRpcUrl = process.env.BSC_RPC_URL
     const avaxRpxUrl = process.env.AVAX_RPC_URL
     const metisRpcUrl = process.env.METIS_RPC_URL
 
     if (chainId === BLOCKCHAIN_TO_CHAIN_ID['ARBITRUM'])
         return new providers.MulticallProvider(
-            new ethers.providers.InfuraProvider(chainId, infuraProjectId)
+            new ethers.providers.StaticJsonRpcProvider(arbitrumRpcUrl, chainId)
         )
     else if (chainId === BLOCKCHAIN_TO_CHAIN_ID['BINANCE'])
         return new providers.MulticallProvider(
