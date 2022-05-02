add43c61a9d28ea3fa95c3991b781ec48247b060
hellodespereaux <hellodespereaux@gmail.com>
2022-05-02 22:41:44 +0200

    fix: adjusted epoch deposit usd

--- helpers/v2/getSsovTvl.js
+++ helpers/v2/getSsovTvl.js
@@ -131,7 +131,7 @@ export default async (ssov) => {
             'totalCollateralBalance'
         ]
 
-        const totalEpochDepositsInUSD = type.includes('CALL') ? ethers.utils.formatUnits(
+        const totalEpochDepositsInUSD = name.includes('CALL') ? ethers.utils.formatUnits(
             totalEpochDeposits.mul(underlyingPrice),
             collateralDecimals + 8
         ) : ethers.utils.formatUnits(totalEpochDeposits, 18);
