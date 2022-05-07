c936bfaf22bf1ea9597fa954d858b813aab84cda
hellodespereaux <hellodespereaux@gmail.com>
2022-05-07 22:34:56 +0200

    chore: switch from infura to ankr

--- helpers/getProvider.js
+++ helpers/getProvider.js
@@ -8,6 +8,7 @@ export default (chainId) => {
     const bscRpcUrl = process.env.BSC_RPC_URL
     const avaxRpxUrl = process.env.AVAX_RPC_URL
     const metisRpcUrl = process.env.METIS_RPC_URL
+    const arbitrumRpcUrl = process.env.ARBITRUM_RPC_URL
 
     if (chainId === BLOCKCHAIN_TO_CHAIN_ID.BSC)
         return new providers.MulticallProvider(
@@ -20,6 +21,10 @@ export default (chainId) => {
     else if (chainId === BLOCKCHAIN_TO_CHAIN_ID.METIS)
         return new providers.MulticallProvider(
             new ethers.providers.StaticJsonRpcProvider(metisRpcUrl, chainId)
+        )
+    else if (chainId === BLOCKCHAIN_TO_CHAIN_ID.ARBITRUM)
+        return new providers.MulticallProvider(
+            new ethers.providers.StaticJsonRpcProvider(arbitrumRpcUrl, chainId)
         )
 
     return new providers.MulticallProvider(
