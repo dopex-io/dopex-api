baeba99987410b4aed06db0556efc194fa85af6e
hellodespereaux <hellodespereaux@gmail.com>
2022-06-09 18:31:43 +0200

    fix: adjust output

--- api/v2/rateVaults/index.js
+++ api/v2/rateVaults/index.js
@@ -2,10 +2,11 @@ import groupBy from 'lodash/groupBy'
 
 import { VAULTS } from '../../../helpers/v2/constants'
 import getIRVaultData from '../../../helpers/v2/getIRVaultData'
+import {utils as ethersUtils} from "ethers/lib/ethers";
 
 export default async (_req, res) => {
     try {
-        const [data] = await Promise.all(
+        const data = await Promise.all(
                 VAULTS.map((vault) => {
                     return getIRVaultData(vault)
                 })
@@ -14,11 +15,10 @@ export default async (_req, res) => {
         const vaultArray = VAULTS.map((item, index) => {
             return {
                 ...item,
-                tvl: data[index].tvl,
-                rate: data[index].rate,
+                tvl: ethersUtils.formatUnits(data[index].tvl, 18),
+                rate: (data[index].rate.toNumber() / 10 ** 8).toString(),
                 currentEpoch: data[index].currentEpoch,
-                totalEpochDeposits: data[index].totalEpochDeposits,
-                underlyingPrice: data[index].underlyingPrice,
+                totalEpochDeposits: ethersUtils.formatUnits(data[index].totalEpochDeposits, 18)
             }
         })
 
