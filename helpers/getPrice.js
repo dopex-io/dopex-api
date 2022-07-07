19f74e46e07166b1f5a4262eda184d262ea831ba
WitherBlock <witherblock@protonmail.com>
2022-07-08 02:15:34 +0530

    refactor: node-fetch to axios

--- helpers/getPrice.js
+++ helpers/getPrice.js
@@ -1,13 +1,13 @@
-import fetch from "node-fetch";
+import axios from 'axios'
 
 export default async (id) => {
-  const price = await fetch(
-    `https://api.coingecko.com/api/v3/simple/price?ids=${id}&vs_currencies=usd,eth`
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      return data[id];
-    });
+    const price = await axios
+        .get(
+            `https://api.coingecko.com/api/v3/simple/price?ids=${id}&vs_currencies=usd,eth`
+        )
+        .then((payload) => {
+            return payload.data[id]
+        })
 
-  return price;
-};
+    return price
+}
