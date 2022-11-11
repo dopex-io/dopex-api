940b5a3afb30c88f71f1be3f6061d78fd864a725
psytama <psy@dopex.io>
2022-11-11 06:37:43 +0400

    feat: added api for farms apy

--- helpers/getFarmTvl.js
+++ helpers/getFarmTvl.js
@@ -2,15 +2,20 @@ import {
     ERC20__factory,
     UniswapPair__factory,
     Addresses,
-    StakingRewards__factory,
+    StakingRewardsV3__factory,
 } from '@dopex-io/sdk'
 import BN from 'bignumber.js'
 
-import { BLOCKCHAIN_TO_CHAIN_ID } from '../helpers/constants'
+import { BLOCKCHAIN_TO_CHAIN_ID } from './constants'
 import getProvider from './getProvider'
 
 const getFarmTvl = async (token, ethPriceFinal) => {
     const contractAddresses = Addresses[BLOCKCHAIN_TO_CHAIN_ID['ARBITRUM']]
+
+    const stakingRewardsAddresses = {
+        'DPX-WETHStakingRewards': '0x1f80C96ca521d7247a818A09b0b15C38E3e58a28',
+        'RDPX-WETHStakingRewards': '0xEb0F03A203F25F08c7aFF0e1b1C2E0EE25Ca29Eb',
+    }
 
     const provider = getProvider(BLOCKCHAIN_TO_CHAIN_ID.ARBITRUM)
 
@@ -22,8 +27,8 @@ const getFarmTvl = async (token, ethPriceFinal) => {
 
     const stakingAsset = token.toUpperCase() + 'StakingRewards'
 
-    const stakingRewardsContract = StakingRewards__factory.connect(
-        contractAddresses[stakingAsset],
+    const stakingRewardsContract = StakingRewardsV3__factory.connect(
+        stakingRewardsAddresses[stakingAsset],
         provider
     )
 
