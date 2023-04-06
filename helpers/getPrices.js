f586f2f6c5af2fca89462de8aa541d7bd1499de9
WitherBlock <witherblock@protonmail.com>
2023-04-06 06:05:05 +0400

    feat: add redis cache for coingecko pricing

--- helpers/getPrices.js
+++ helpers/getPrices.js
@@ -1,15 +1,41 @@
 import axios from 'axios'
 
-export default async (ids) => {
-    const prices = await axios
-        .get(
-            `https://api.coingecko.com/api/v3/simple/price?ids=${ids.join(
-                ','
-            )}&vs_currencies=usd`
-        )
-        .then((payload) => {
-            return ids.map((id) => payload.data[id].usd)
-        })
+import redis from './redis'
 
-    return prices
+export default async (ids) => {
+    const prices = {}
+
+    const uncachedIds = []
+
+    for (let i = 0; i < ids.length; i++) {
+        const id = ids[i]
+
+        const price = await redis.get(id)
+
+        if (price) {
+            prices[id] = price
+        } else {
+            uncachedIds.push(id)
+        }
+    }
+
+    if (uncachedIds.length > 0) {
+        await axios
+            .get(
+                `https://api.coingecko.com/api/v3/simple/price?ids=${uncachedIds.join(
+                    ','
+                )}&vs_currencies=usd`
+            )
+            .then(async (payload) => {
+                for (let i = 0; i < uncachedIds.length; i++) {
+                    const id = uncachedIds[i]
+
+                    prices[id] = payload.data[id].usd
+
+                    await redis.set(id, prices[id], { ex: 60 })
+                }
+            })
+    }
+
+    return ids.map((id) => prices[id])
 }
